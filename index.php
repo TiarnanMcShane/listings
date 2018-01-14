41e848125e0aeabb108fbc955e5d22433fdb7dc7
TiarnanMcShane <tierneymcshane@googlemail.com>
2018-01-14 19:24:10 +1100

    Separating the results data
    Tapping into the results aray to separate each listing.

--- index.php
+++ index.php
@@ -11,8 +11,17 @@
 	$json_data = file_get_contents('data.json', true); // read the contents of the json file to a string.
 
 	$listings = json_decode($json_data, true); // decodes the string into a PHP variable, with 'true' being set to convert the object into an array.
-	
-	print_r($listings); // output our newly created $listings variable
-	
+	$results = $listings['results']; // new variable to store 'results' data part of the feed.
+	$saved = $listings['saved']; // new variable to store 'saved' data part of the feed.
+
+	echo '<div class="results">';
+	echo '<h2>Results</h2>';
+	// tap into the $results array to start listing each result separately
+	foreach($results as $key=>$value){
+	    print_r($value); 
+	    echo '<br><br>';
+	}
+	echo '</div>';
+
 	?>
 </body>
