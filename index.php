d74e7c32c5b9ac54f650480cde0914525b0eae80
TiarnanMcShane <tierneymcshane@googlemail.com>
2018-01-14 19:15:17 +1100

    First Output
    Lets output our $listings variable to see what data we are dealing with and what arrays to tap into to start displaying our data in a cleaner format.

--- index.php
+++ index.php
@@ -7,5 +7,12 @@
 	<link rel="stylesheet" type="text/css" href="style.css"> <!-- include our stylesheet -->
 </head>
 <body>
+	<?php 
+	$json_data = file_get_contents('data.json', true); // read the contents of the json file to a string.
 
+	$listings = json_decode($json_data, true); // decodes the string into a PHP variable, with 'true' being set to convert the object into an array.
+	
+	print_r($listings); // output our newly created $listings variable
+	
+	?>
 </body>
