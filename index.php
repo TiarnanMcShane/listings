2a80f9fd2033bd674ea4a248a388cdf22cc0b140
TiarnanMcShane <tierneymcshane@googlemail.com>
2018-01-14 19:00:51 +1100

    Index & Stylesheet
    Adding our initial files to the repository

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying a list of properties from a json feed.
+ */
+?>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css"> <!-- include our stylesheet -->
+</head>
+<body>
+
+</body>
